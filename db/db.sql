145282ee564065ec3f970887f80f4651416934d2
skarpecki <skarpecki@outlook.com>
2020-06-08 13:47:26 +0200

    Minor code chagnes, added foreign keys

--- db/db.sql
+++ db/db.sql
@@ -4,6 +4,8 @@ grant all privileges on szkola_podstawowa_db.* to administrator;
 
 use szkola_podstawowa_db;
 
+set foreign_key_checks = 0;
+
 drop table if exists `osoba`;
 create table `osoba` (
 	`osoba_id` int not null auto_increment,
@@ -12,8 +14,8 @@ create table `osoba` (
     `nr_telefonu` varchar(11) not null,
     `pesel` varchar(11) not null,
     `email` varchar(100) not null,
-	`klasa_id` int not null, #fk_klasa
-    `zajecia_id` int, #fk_zajecia - charakterysytczne dla nauczyciela
+    `typ` enum ('rodzic','uczen','nauczyciel'),
+	`klasa_id` int, #fk_klasa
     `uczen_id` int, #fk_osoba -> id_ucznia -> charakterysytyczne dla rodzica
     constraint primary key(`osoba_id`) 
 );
@@ -31,8 +33,8 @@ create table `ocena`(
 	`ocena_id` int not null auto_increment,
     `wartosc` enum ('1.0','2.0','2.5','3.0', '3.5', '4.0', '4.5', '5.0') NOT NULL,
 	`waga` int not null,
-    `osoba_id` int not null, #fk_uczen
-    `przedmiot_id` int not null, #fk_przedmiot
+    `uczen_id` int not null, #fk_uczen
+    `zajecia_id` int not null, #fk_zajecia
 	constraint primary key(`ocena_id`)
 );
 
@@ -40,7 +42,6 @@ drop table if exists `przedmiot`;
 create table `przedmiot`(
 	`przedmiot_id` int not null auto_increment,
     `nazwa_przedmiotu` varchar(50) not null,
-    `grupa_przedmiotowa_id` int not null,
     constraint primary key(`przedmiot_id`)
 );
 
@@ -50,7 +51,7 @@ create table `zajecia`(
     `dzien` enum ('Poniedzialek','Wtorek','Sroda','Czwartek','Piatek') not null,
     `godzina_start` time not null,
     `godzina_koniec` time not null,
-    `osoba_id` int not null, #fk_nauczyciel
+    `nauczyciel_id` int not null, #fk_nauczyciel
     `klasa_id` int not null, #fk_klasa
     `przedmiot_id` int not null, #fk_przedmiot
     `sala_id` int not null, #fk_klasa
@@ -64,5 +65,18 @@ create table `sala`(
     constraint primary key(`sala_id`)
 );
 
+set foreign_key_checks = 1;
 
+alter table `osoba`
+	add foreign key (`klasa_id`) references `klasa`(`klasa_id`),
+    add foreign key (`uczen_id`) references `osoba`(`osoba_id`);
 
+alter table `ocena`
+	add foreign key (`uczen_id`) references `osoba`(`osoba_id`),
+    add foreign key (`zajecia_id`) references `zajecia`(`zajecia_id`);
+    
+alter table `zajecia`
+	add foreign key (`nauczyciel_id`) references `osoba`(`osoba_id`),
+    add foreign key (`klasa_id`) references `klasa`(`klasa_id`),
+    add foreign key (`przedmiot_id`) references `przedmiot`(`przedmiot_id`),
+    add foreign key (`sala_id`) references `sala`(`sala_id`);
